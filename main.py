27bf03baf0746ee5e5185fb4ba4c0e2c81a16f02
pnv28 <72593874+pnv28@users.noreply.github.com>
2023-04-02 22:18:57 +0545

    Basic Setup for bot to come onlien

---
+++ main.py
@@ -0,0 +1,5 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+bot.run("MTA5MjEwNDYwODg3MDA0MzcwOA.GKzNws.0GhFfov1ldCRVt3YkF7ShZ5z4H2sDWCgXmrPfc")
