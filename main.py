dfb03f64c22551253ce523fda8a30cd55ee6b09a
pnv28 <72593874+pnv28@users.noreply.github.com>
2023-04-02 22:34:20 +0545

    More basic stuff

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import discord
 from discord.ext import commands
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
-bot.run("MTA5MjEwNDYwODg3MDA0MzcwOA.GKzNws.0GhFfov1ldCRVt3YkF7ShZ5z4H2sDWCgXmrPfc")
+@bot.event
+async def on_ready():
+    print("The bot is now online")
+
+@bot.command()
+async def lol(ctx):
+    await ctx.send(".lol")
+
+bot.run("MTA5MjEwNDYwODg3MDA0MzcwOA.GuTTHT.feI5_ZwmwjW0lhQRFUgKra-f751e5LZ3BQR4-g")
