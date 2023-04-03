045fd7516eb2a7896902cebd63d6299a7422e4f3
pnv28 <72593874+pnv28@users.noreply.github.com>
2023-04-03 10:58:32 +0545

    Made the bot join and leave channel via command

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ async def lol(ctx):
     id = ctx.message.author.id;
     await ctx.send(f"I am laughing at you <@{id}>")
 
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    if ctx.author.voice is None:
+        await ctx.reply("User is not in the voice channel")
+        return
+    await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    voice_client = ctx.guild.voice_client
+    if(voice_client.is_connected):
+        await voice_client.disconnect()
+
 bot.run("MTA5MjEwNDYwODg3MDA0MzcwOA.GuTTHT.feI5_ZwmwjW0lhQRFUgKra-f751e5LZ3BQR4-g")
