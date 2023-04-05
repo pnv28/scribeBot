eaa667e1c4d1c21ed7a41741ffdf1bcb054e23b2
pnv28 <72593874+pnv28@users.noreply.github.com>
2023-04-05 22:04:56 +0545

    It plays audio when you use .join or when the bot auto joins

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import os
-from discord.ext import commands
-import asyncio
 import logging
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+import asyncio
+import time
 logging.basicConfig(level=logging.DEBUG)
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
@@ -13,7 +15,9 @@ async def on_ready():
 @bot.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:    
-        await after.channel.connect()
+        voice = await after.channel.connect()
+        source = FFmpegPCMAudio('sed.wav')
+        player = voice.play(source)
     if before.channel is not None and after.channel is None:
         voice_client = member.guild.voice_client
         if voice_client is not None and len(voice_client.channel.members) == 1:
@@ -26,39 +30,20 @@ async def lol(ctx):
 
 @bot.command()
 async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect()
+        source = FFmpegPCMAudio('sed.wav')
+        player = voice.play(source)
+    else:
+        await ctx.send("You are not connected to a voice channel.")
 
 @bot.command()
 async def leave(ctx):
     voice_client = ctx.guild.voice_client
-    if not voice_client:
-        return
-    if(voice_client.is_connected):
+    if voice_client.is_connected():
         await voice_client.disconnect()
-
-@bot.command()
-async def record(ctx):
-    if ctx.author.voice is None:
-        await ctx.send("You need to join a voice channel first.")
-        return
-
-    voice_channel = ctx.author.voice.channel
-    voice_client = await voice_channel.connect()
-
-    audio_source = discord.FFmpegPCMAudio("recording.wav")
-    voice_client.play(audio_source)
-
-    await ctx.send("Recording started.")
-
-@bot.command()
-async def stop(ctx):
-    voice_client = ctx.voice_client
-    if voice_client is not None:
-        await voice_client.disconnect()
-        await ctx.send("Recording stopped.")
     else:
-        await ctx.send("I'm not in a voice channel.")
-
+        await ctx.send("The bot is not connected to a voice channel.")
 
 bot.run("")
